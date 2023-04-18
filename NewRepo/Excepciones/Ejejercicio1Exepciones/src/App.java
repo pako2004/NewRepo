4fb69763fd3ccc2904a804e945c40bdd6af70675
pako2004 <lrodmat346y@g.educaand.es>
2023-04-18 14:32:46 +0200

    ioik

--- NewRepo/Excepciones/Ejejercicio1Exepciones/src/App.java
+++ NewRepo/Excepciones/Ejejercicio1Exepciones/src/App.java
@@ -1,5 +1,22 @@
+import clase.utiles.LecturaTeclado;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        LecturaTeclado sc = new LecturaTeclado();
+        int maximo = 0;
+
+        for (int i = 0; i < 6; i++) {
+            int num = (int)(Math.random()*3);
+            
+            System.out.println("Introduce un numeroi");
+            int numero = sc.leerInt();
+            if(numero > maximo)
+            {
+                maximo = numero;
+            }
+        }
+        System.out.println("El maximo es : "+maximo);
     }
 }
